f6f64b5b90335050958aefe9c6c413e796f18b6b
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2023-07-25 09:51:55 +0200

    Restructure dependencies (#1)
    * vendor: remove vendored flexfloat library
    
    * deps: Add the flexfloat library as a submodule
    
    * flexfloat: Adapt the flexfloat package for the updated library version
    
    * vendor: Remove unused SoftFloat-3e library

--- build/build.rs
+++ build/build.rs
@@ -3,14 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod runtime;
-mod softfloat;
 
 fn main() {
-    // Prevent cargo from re-building everything by default.
-    // Other subcommands can still emit their own rerun-if-changed lines.
-    println!("cargo:rerun-if-changed=build/softfloat.rs");
-
     // Build the components.
-    softfloat::build();
     runtime::build();
 }
--- flexfloat/build.rs
+++ flexfloat/build.rs
@@ -2,24 +2,25 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-const FLEXFLOAT_DIR: &str = "../vendor/flexfloat";
+const FLEXFLOAT_DIR: &str = "../deps/flexfloat";
+
+use cmake::Config;
 
 fn main() {
-    let src = ["../vendor/flexfloat/src/flexfloat.c"];
+    let src = ["../deps/flexfloat/src/flexfloat.c"];
 
     // Ensure that we rebuild whenever any of the input files changes.
     for f in &src {
         println!("cargo:rerun-if-changed={}", f);
     }
 
-    let mut builder = cc::Build::new();
-    let build = builder
-        .files(src.iter())
-        .include(format!("{}/include", FLEXFLOAT_DIR))
-        .flag("-std=c11")
-        .flag("-O3")
-        .warnings(false);
-    // .define("FLEXFLOAT_FLAGS", None);
+    let dst = Config::new(FLEXFLOAT_DIR)
+        .define("BUILD_TESTS", "OFF")
+        .define("BUILD_EXAMPLES", "OFF")
+        .no_build_target(true)
+        .very_verbose(true)
+        .build();
 
-    build.compile("flexfloat");
+    println!("cargo:rustc-link-search={}/build", dst.display());
+    println!("cargo:rustc-link-lib=flexfloat");
 }
